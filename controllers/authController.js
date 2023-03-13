53e99481e3dadf6089dcea6e0a5672d2c33eccb6
Emi <emi.lng711@gmail.com>
2023-03-13 18:14:24 -0300

    commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,78 +1,86 @@
 const bcrypt = require("bcryptjs");
 const { User } = require("../models");
 const formidable = require("formidable");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 // Singup Page.
 async function register(req, res) {
-    res.render("users/register")
+  res.render("users/register");
 }
 
 // Login Page.
 async function login(req, res) {
-    res.render("users/login")
+  res.render("users/login");
 }
 
 // Display a listing of the resource.
 async function logout(req, res) {
-    req.logout(function (err) {
-        if (err) { return next(err); }
-        res.redirect("/login");
-    });
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
 }
 
 //  Crear usuario en la DB
 function createUser(req, res) {
-    const form = formidable({
-        uploadDir: __dirname + "/../public/img",
-        keepExtensions: true,
-    });
-    form.parse(req, async (err, fields, files) => {
-        const users = await User.find()
-        if (fields.username === "" || fields.email === "" || fields.password === "" || fields.firstname === "") {
-            req.flash('text', 'Rellena todos los campos.');
-            res.redirect("back")
-        } else {
-            const unavailableUser = users.some((u) => u.username === fields.username || u.email === fields.email)
-            if (unavailableUser) {
-                req.flash('text', 'El usuario ya existe.');
-                res.redirect("back")
-            } else {
-                await User.create({
-                    firstname: fields.firstname,
-                    lastname: fields.lastname,
-                    email: fields.email,
-                    username: fields.username,
-                    image: files.image.newFilename,
-                    password: fields.password /* await bcrypt.hash(fields.password, 8) */
-                })
-                /* user.save() */
-                return res.redirect("/");
-            }
-        }
-    })
+  const form = formidable({
+    uploadDir: __dirname + "/../public/img",
+    keepExtensions: true,
+  });
+  form.parse(req, async (err, fields, files) => {
+    const users = await User.find();
+    if (
+      fields.username === "" ||
+      fields.email === "" ||
+      fields.password === "" ||
+      fields.firstname === ""
+    ) {
+      req.flash("text", "Rellena todos los campos.");
+      res.redirect("back");
+    } else {
+      const unavailableUser = users.some(
+        (u) => u.username === fields.username || u.email === fields.email
+      );
+      if (unavailableUser) {
+        req.flash("text", "El usuario ya existe.");
+        res.redirect("back");
+      } else {
+        await User.create({
+          firstname: fields.firstname,
+          lastname: fields.lastname,
+          email: fields.email,
+          username: fields.username,
+          image: files.image.newFilename,
+          password: fields.password /* await bcrypt.hash(fields.password, 8) */,
+        });
+        /* user.save() */
+        return res.redirect("/");
+      }
+    }
+  });
 }
 
 async function loginUser(req, res) {
-    console.log("REQ.BODY: ", req.body);
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-        console.log("El usuario  no  existe");
-    }
-    const match = await bcrypt.compare(req.body.password, user.password);
-    if (!match) {
-        console.log("La pass es inválida");
-    }
-    try {
-        const payload = { id: user.id }
-        const secret = 'privatekey'
-        const token = jwt.sign(payload, secret);
-        res.json({ user: user.username, user: user.id, token });
-    } catch (e) {
-        res.status(400).send(e);
-    }
-};
-
+  console.log("REQ.BODY: ", req.body);
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    console.log("El usuario  no  existe");
+  }
+  const match = await bcrypt.compare(req.body.password, user.password);
+  if (!match) {
+    console.log("La pass es inválida");
+  }
+  try {
+    const payload = { id: user.id };
+    const secret = "privatekey";
+    const token = jwt.sign(payload, secret);
+    res.json({ user: user.username, userId: user.id, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+}
 
 /* const loginPassport = passport.authenticate("local", {
     successRedirect: "/",
@@ -80,12 +88,10 @@ async function loginUser(req, res) {
     failureFlash: true
 }); */
 
-
 module.exports = {
-    register,
-    login,
-    loginUser,
-    logout,
-    createUser,
-
+  register,
+  login,
+  loginUser,
+  logout,
+  createUser,
 };
--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 require("dotenv").config();
-const cors = require('cors')
+const cors = require("cors");
 const path = require("path");
 const methodOverride = require("method-override");
 const express = require("express");
-const flash = require('express-flash');
+const flash = require("express-flash");
 
 const sessions = require("./sessions");
 const routes = require("./routes");
-const passport = require("./passport");
 
 const APP_PORT = process.env.APP_PORT || 8000;
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.json());
 
-
 sessions(app);
-passport(app);
 routes(app);
 
 app.use(flash());
@@ -35,7 +32,9 @@ app.listen(APP_PORT, () => {
 process.on("SIGINT", function () {
   const { mongoose } = require("./db");
   mongoose.connection.close(function () {
-    console.log("Mongoose default connection is disconnected due to application termination.\n");
+    console.log(
+      "Mongoose default connection is disconnected due to application termination.\n"
+    );
     process.exit(0);
   });
 });
