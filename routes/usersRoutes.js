c1be34c97e27187b9006054deab1c212732aec11
Emi <emi.lng711@gmail.com>
2023-03-16 12:33:43 -0300

    Route-random

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const { expressjwt: checkJwt } = require("express-jwt");
 const usersController = require("../controllers/usersController");
 
-
 router.post("/users", usersController.store);
 router.post("/tokens", usersController.token);
 
-router.use(checkJwt({ secret: "privateKey", algorithms: ["HS256"] }))
+router.use(checkJwt({ secret: "privateKey", algorithms: ["HS256"] }));
 
+router.get("/random", usersController.randomUser);
 router.get("/:username", usersController.show);
 router.get("/:username/followers", usersController.followers);
 router.get("/:username/following", usersController.following);
@@ -17,8 +17,5 @@ router.patch("/banner", usersController.bannerEdit);
 // Hacer Toggle de Follow
 router.patch("/:id/follow", usersController.follow);
 router.patch("/:id/unfollow", usersController.unfollow);
-
-
-
 
 module.exports = router;
