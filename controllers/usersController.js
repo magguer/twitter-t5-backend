035d9a405ff6a2a69219e09dada461f3cd5c0483
magguer <martintelaguer@gmail.com>
2023-03-20 18:40:38 -0300

    sub-populate-tweets

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -148,11 +148,12 @@ async function unfollow(req, res) {
 async function show(req, res) {
   const userProfile = await User.findOne({
     username: req.params.username,
-  }).populate({ path: "tweets", options: { sort: { createdAt: -1 } } });
+  }).populate({ path: "tweets", populate: "likes", options: { sort: { createdAt: -1 } } });
   return res.json({
     userProfile,
   });
 }
+
 
 // GET - Followers de Usuario
 async function followers(req, res) {
