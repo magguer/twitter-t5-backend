672fd69eed23bf44e90e85dbf1c51fbc30e488a7
magguer <martintelaguer@gmail.com>
2023-03-30 12:40:50 -0300

    no-express-ejs-session

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
-const methodOverride = require("method-override");
 const express = require("express");
 
 const routes = require("./routes");
@@ -10,14 +9,11 @@ const APP_PORT = process.env.APP_PORT || 8000;
 const app = express();
 
 app.use(cors());
-app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
 app.use(express.json());
 
 routes(app);
-
 
 app.listen(APP_PORT, () => {
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
